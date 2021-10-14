628791b38552bdbab088b574cae65254c195855a
Danis Dwi Prasetyanto <danis.prasetyanto@binus.edu>
2021-10-14 13:03:06 +0700

    first commit

---
+++ src/screens/Counter.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View, Text, SafeAreaView} from 'react-native'
+
+const CounterScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>Haloo Darkness my old friend</Text>
+        </SafeAreaView>
+    )
+}
+export default CounterScreen
